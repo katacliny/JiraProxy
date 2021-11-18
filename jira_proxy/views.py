0dbd135539766e1765cdff660ee9589aacf7f0f5
alex <nolodelatorre@gmail.com>
2021-11-18 19:02:26 +0100

    adding post

--- jira_proxy/views.py
+++ jira_proxy/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from urllib import parse
 from django.views.decorators.csrf import csrf_exempt
+import requests
 
 @csrf_exempt
 def proxy_view(request):
@@ -13,6 +14,9 @@ def proxy_view(request):
     # print(request.get_raw_uri())
     parsed = parse.urlsplit(request.get_full_path())
     print(parse.parse_qs(parsed.query))
+    url = 'http://51.75.147.46:1337/jira'
+    body = request.body
+    requests.post(url, data = body)
     
     
     return HttpResponse("OK")
